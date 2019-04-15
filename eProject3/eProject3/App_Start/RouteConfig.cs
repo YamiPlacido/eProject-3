b6a1167dfb0e20b4e5bd72bb2598a0a31bf39298
YamiPlacido <40970788+YamiPlacido@users.noreply.github.com>
2019-04-15 21:33:22 +0700

    Update RouteConfig.cs

--- eProject3/eProject3/App_Start/RouteConfig.cs
+++ eProject3/eProject3/App_Start/RouteConfig.cs
@@ -16,6 +16,7 @@ namespace eProject3
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
                 namespaces: new[] { "eProject3.Controllers" }
             );
         }
